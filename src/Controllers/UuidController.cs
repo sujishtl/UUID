efc17073c18a52b8732876f093cd0bf814f0f94b
Sujish Lakshmanan <sujishtl@gmail.com>
2021-10-21 16:47:28 +1100

    Code to get the longest sequence with all unit test cases working

--- src/Controllers/UuidController.cs
+++ src/Controllers/UuidController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -14,11 +15,12 @@ namespace DotnetDockerIntegrationTests.Controllers
     [ApiController]
     public class UuidController : ControllerBase
     {
-        
+
         private readonly IValidator<string> _validator;
 
-        public UuidController( IValidator<string> validator)        {
-            
+        public UuidController(IValidator<string> validator)
+        {
+
             _validator = validator;
         }
 
@@ -28,11 +30,40 @@ namespace DotnetDockerIntegrationTests.Controllers
         public async Task<IActionResult> GetSequence(string sequence)
         {
             string[] sequenceSplits = Regex.Split(sequence, " ");
-      
-            int[] seqNumbers = Array.ConvertAll(sequenceSplits, int.Parse);           
+            int[] seqNumbers = Array.ConvertAll(sequenceSplits, int.Parse);
+            int number1 = 0;
+            List<int> longSeq = new List<int>();
+            List<int> finalLongSeq = new List<int>();
 
-            string returnValue="";
-            foreach(var x in seqNumbers)
+            for (int i = 0; i < seqNumbers.Length; i++)
+            {
+                if ((i + 1) == seqNumbers.Length)
+                {
+                    if (number1 < seqNumbers[i])
+                    {
+                        number1 = seqNumbers[i];
+                        longSeq.Add(number1);
+                    }
+                }
+                else
+                {
+                    number1 = seqNumbers[i];
+                    longSeq.Add(number1);
+                }
+                if (((i + 1) == seqNumbers.Length) || number1 > seqNumbers[i + 1])
+                {
+                    if (longSeq.Count > finalLongSeq.Count)
+                    {
+                        finalLongSeq.Clear();
+                        finalLongSeq.AddRange(longSeq);
+
+                    }
+                    longSeq.Clear();
+                }
+            }
+
+            string returnValue = "";
+            foreach (var x in finalLongSeq)
             {
                 returnValue = returnValue + " " + x;
             }
